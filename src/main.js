faf78ab9679cb50614396d94c431affef2841908
Christoph <christoph.schneider@mail.ch>
2020-06-10 00:04:26 +0200

    removed unused code

--- src/main.js
+++ src/main.js
@@ -4,9 +4,6 @@ var path = require( 'path' );
 if( !fs.existsSync( path.join( __dirname, '../data' ) ) )
     fs.mkdirSync( path.join( __dirname, '../data' ) );
 
-if( !fs.existsSync( path.join( __dirname, '../data/scores.json' ) ) )
-    fs.writeFileSync( path.join( __dirname, '../data/scores.json' ), '{}' );
-
 require( './db.js' ).init();
 
 require( './server.js' );
